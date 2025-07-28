763fdf74cb07927cb999d99beea3295c922152b1
olbbemi <shaiya1@naver.com>
2025-07-28 16:00:13 +0900

    update clang-format cfg

--- src/util_thread_pool.h
+++ src/util_thread_pool.h
@@ -16,29 +16,25 @@
 
 #include "util_logger.h"
 
-namespace util
-{
+namespace util {
 /* ====================================================================== */
 /* ========================== DEFINE & ENUM ============================= */
 /* ====================================================================== */
 const std::uint16_t FD_EVENT_TYPE_SIZE = 8;
-using EVENT_FD = std::int32_t;
+using EVENT_FD                         = std::int32_t;
 
 /* ====================================================================== */
 /* ========================== CLASS & STRUCT ============================ */
 /* ====================================================================== */
 
-class thread_pool_c
-{
-  public:
+class thread_pool_c {
+public:
     bool create_pool(const std::string &identification, std::uint16_t max_cnt);
 
     template <typename Func, typename... Args>
-    bool async_dispatch(Func &&func, Args &&...args)
-    {
+    bool async_dispatch(Func &&func, Args &&...args) {
         // when shutdown flag is true, no more tasks should be enqueued.
-        if (bool shutdown = _shutdown.load(); true == shutdown)
-        {
+        if (bool shutdown = _shutdown.load(); true == shutdown) {
             U_LOG_ROTATE_FILE(util::LOG_LEVEL::DEBUG,
                               "[{}] thread_pool is shutdown.", _identification);
             return false;
@@ -47,6 +43,7 @@ class thread_pool_c
         // make task with function and variable.
         auto task =
             std::bind(std::forward<Func>(func), std::forward<Args>(args)...);
+
         {
             std::lock_guard<std::mutex> lock_obj(_task_mutex);
             _task_queue.push(task);
@@ -54,44 +51,44 @@ class thread_pool_c
 
         // awake producer thread with event_fd.
         std::uint64_t value = 1;
-        static_assert(
-            sizeof(value) == FD_EVENT_TYPE_SIZE,
-            "[ERROR] value-size mismatch for write(event_fd). need_size: "
-            "FD_EVENT_TYPE_SIZE");
+        static_assert(sizeof(value) == FD_EVENT_TYPE_SIZE,
+                      "[ERROR] value-size mismatch for write(event_fd). "
+                      "need_size: FD_EVENT_TYPE_SIZE");
+
         ssize_t result = write(_event_fd, &value, sizeof(value));
         return true;
     }
 
-  public:
+    /* <-- special member functions --> */
     thread_pool_c() = default;
     ~thread_pool_c();
 
-    thread_pool_c(const thread_pool_c &&rhs) = delete;
+    thread_pool_c(const thread_pool_c &&rhs)           = delete;
     thread_pool_c &operator=(const thread_pool_c &rhs) = delete;
 
-    thread_pool_c(thread_pool_c &&rhs) = delete;
-    thread_pool_c &operator=(thread_pool_c &&rhs) = delete;
+    thread_pool_c(thread_pool_c &&rhs)                 = delete;
+    thread_pool_c &operator=(thread_pool_c &&rhs)      = delete;
 
-  private:
+private:
     void _task_consumer_thread(std::promise<void> ready_signal,
-                               std::uint16_t index);
+                               std::uint16_t      index);
     void _task_producer_thread();
     bool _check_exec_right_now();
 
-  private:
+private:
     std::atomic_bool _shutdown;
     std::atomic_bool _ready_task;
-    std::uint16_t _max_cnt;
-    EVENT_FD _event_fd;
+    std::uint16_t    _max_cnt;
+    EVENT_FD         _event_fd;
 
-    std::string _identification;
-    std::vector<std::int64_t> _consumer_elapsed_time;
-    std::vector<std::thread> _task_consumer_pool;
-    std::queue<std::function<void()>> _task_queue;
+    std::string                                 _identification;
+    std::vector<std::int64_t>                   _consumer_elapsed_time;
+    std::vector<std::thread>                    _task_consumer_pool;
+    std::queue<std::function<void()>>           _task_queue;
     std::map<std::thread::id, std::atomic_bool> _consumer_mgr;
 
-    std::mutex _task_mutex;
-    std::mutex _mgr_mutex;
+    std::mutex  _task_mutex;
+    std::mutex  _mgr_mutex;
     std::thread _task_producer;
 
     std::condition_variable _task_cv;
